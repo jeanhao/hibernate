7a1c2e512f144f3bc12fb82a9db87bb69980358e
zenghao <jeanheo@foxmail.com>
2016-03-28 00:23:46 +0800

    添加了spring整合hibernate的示例代码，实现向数据库插入一条记录

--- helloHibernate/src/com/zeng/test/Test1.java
+++ helloHibernate/src/com/zeng/test/Test1.java
@@ -8,11 +8,13 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.zeng.model.User;
 
 public class Test1 {
-	@Test
+//	@Test
 	public void test1(){
 		//解析我们在hibernate.cfg.xml中的配置
 		Configuration configuration = new Configuration().configure();
@@ -27,6 +29,19 @@ public class Test1 {
 		Transaction transaction = session.beginTransaction();//开启一个新的事务
 		User user = new User();
 		user.setName("zengh");
+		session.save(user);
+		transaction.commit();//提交事务
+	}
+	
+	@Test
+	public void test2(){
+		ApplicationContext ac = new ClassPathXmlApplicationContext("spring-datasource.xml");
+		SessionFactory sessionFactory = (SessionFactory) ac.getBean("sessionFactory");
+Session session = sessionFactory.openSession();//从会话工厂获取一个session
+		
+		Transaction transaction = session.beginTransaction();//开启一个新的事务
+		User user = new User();
+		user.setName("hello spring");
 		session.save(user);
 		transaction.commit();//提交事务
 	}
